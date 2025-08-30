1a4cd5bffc9e10b86d95e37de4002a3de8dd0b32
Shivam Kumar <shivamkumar28499@gmail.com>
2025-08-30 12:15:11 +0530

    LeetCode-416

---
+++ DP/LettCode-416.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[205][20005];
+     bool helper(int i,vector<int>& nums,int sum,int target){
+        if(sum==target) return true;
+        if(i>=nums.size()) return false;
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        bool ans1 = false;
+        if(sum+nums[i]<=target){
+            ans1 =  helper(i+1,nums,sum+nums[i],target);
+        }
+        bool ans2 = helper(i+1,nums,sum,target);
+        return dp[i][sum] = ans1 || ans2;
+     }
+
+    bool canPartition(vector<int>& nums) {
+        int totalSum = 0;        
+        for(int i=0;i<nums.size();i++) totalSum += nums[i];
+        int target = totalSum/2;
+        if(totalSum%2!=0) return false;
+        memset(dp,-1,sizeof dp);
+        return helper(0,nums,0,target);
+    }
+};
